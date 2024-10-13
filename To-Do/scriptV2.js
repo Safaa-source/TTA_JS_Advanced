0a8ca5383bb0217fe319424df9810c212e740589
Safaa-source <safay410@gmail.com>
2024-10-13 23:44:53 +0200

    feat:finalize the featured todo list

--- To-Do/scriptV2.js
+++ To-Do/scriptV2.js
@@ -1,4 +1,5 @@
 let tasks = ["Buy milk", "Clean the room", "Go to the gym"];
+let completedTasks = []
 
 const displayTasks = () => {
     let taskDisplay = document.querySelector('#taskDisplay');
@@ -19,6 +20,13 @@ const displayTasks = () => {
         taskLinks.classList.add('task-links');
 
         // create the update and delete buttons
+        const doneLink = document.createElement('a');
+        doneLink.href = "#";
+        doneLink.textContent = 'Mark as Done';
+        doneLink.classList.add('text-green-500', 'mr-4');
+        doneLink.addEventListener('click', () => markAsDone(index));
+        taskLinks.appendChild(doneLink);
+
         const updateButton = document.createElement('a');
         updateButton.href = '#';
         updateButton.textContent = 'Update';
@@ -33,13 +41,14 @@ const displayTasks = () => {
         deleteButton.addEventListener('click', () => deleteTask(index));
         taskLinks.appendChild(deleteButton);
 
-        taskItem.appendChild(taskLinks)
+        taskItem.appendChild(taskLinks);
         taskDisplay.appendChild(taskItem);
     })
 }
 
 const saveTaskToLocalStorage = () => {
     localStorage.setItem('tasks', JSON.stringify(tasks));
+    localStorage.setItem('completedTasks', JSON.stringify(completedTasks));
 }
 
 const addTask = () => {
@@ -54,6 +63,74 @@ const addTask = () => {
     }
     else{
         alert('Please enter a task')
+    }
+}
+
+const markAsDone = (index) => {
+    const task = tasks.splice(index,1)[0];
+    completedTasks.push(task);
+    saveTaskToLocalStorage();
+    displayTasks();
+    displayCompletedTasks();
+}
+
+const displayCompletedTasks = () => {
+    const completedTasksSection = document.querySelector('#completedTasks');
+    const completedTaskDisplay = document.querySelector('#completedTaskDisplay');
+    completedTaskDisplay.innerHTML = '';
+
+    if (completedTasks.length > 0){
+        completedTasksSection.classList.remove('hidden');
+    }
+    else{
+        completedTasksSection.classList.add('hidden');
+    }
+
+    completedTasks.forEach((task, index) => {
+        const taskItem = document.createElement('li');
+        taskItem.classList.add('bg-green-200', 'p-2', 'm-2', 'flex', 'rounded-lg', 'justify-between', 'list-none');
+
+        const taskText = document.createElement('span');
+        taskText.textContent = task;
+        taskText.classList.add('line-through');
+        taskItem.appendChild(taskText);
+
+        const taskLinks = document.createElement('div');
+        taskLinks.classList.add('task-links');
+
+        const undoLink = document.createElement('a');
+        undoLink.href = '#';
+        undoLink.textContent = 'Undo';
+        undoLink.classList.add('text-blue-500', 'mr-4');
+        undoLink.addEventListener('click', () => undoCompletedTask(index));
+        taskLinks.appendChild(undoLink);
+
+        const deleteButton = document.createElement('a');
+        deleteButton.href = '#';
+        deleteButton.textContent = 'Delete';
+        deleteButton.classList.add('text-red-500');
+        deleteButton.addEventListener('click', () => deleteCompletedTask(index));
+        taskLinks.appendChild(deleteButton);
+
+        taskItem.appendChild(taskLinks);
+        completedTaskDisplay.appendChild(taskItem);
+
+    })
+}
+
+const undoCompletedTask = (index) => {
+    const task = completedTasks.splice(index, 1)[0];
+    tasks.push(task);
+    saveTaskToLocalStorage();
+    displayTasks();
+    displayCompletedTasks();
+}
+
+const deleteCompletedTask = (index) => {
+    if (confirm('Are you sure you want to delete this task')){
+        completedTasks.splice(index, 1);
+        saveTaskToLocalStorage();
+        displayCompletedTasks();
     }
 }
 
@@ -78,10 +155,15 @@ const deleteTask = (index) => {
 
 const loadTaskFromLocalStorage = () => {
     const storedTasks = localStorage.getItem('tasks');
-
+    const storedCompletedTasks = localStorage.getItem('completedTasks')
     if(storedTasks){
         tasks = JSON.parse(storedTasks);
         displayTasks();
+    }
+
+    if(storedCompletedTasks){
+        completedTasks.JSON.parse(storedCompletedTasks);
+        displayCompletedTasks();
     }
 }
 
